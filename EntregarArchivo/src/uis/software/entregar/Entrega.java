b12b439a5d9b51810c22605ad5bb06ac6c9645f0
Gabriel Pedraza Ferreira <gabo_pedraza@yahoo.com>
2016-04-19 16:02:30 -0500

    Version del servicio web creando el JSON desde el codigo y no del archivo

--- EntregarArchivo/src/uis/software/entregar/Entrega.java
+++ EntregarArchivo/src/uis/software/entregar/Entrega.java
@@ -5,13 +5,14 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
- 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
  
 @Path("/")
@@ -19,9 +20,45 @@ public class Entrega {
  
 	@GET
 	@Path("/consultar")
-	@Produces(MediaType.APPLICATION_JSON)
-	
+	@Produces(MediaType.APPLICATION_JSON)	
 	public Response verifyRESTService(InputStream incomingData) {
+		String string = "";
+		JSONObject objeto=buildResponse("5", "facil", "soleado, feliz");
+
+		// Retorna el objeto
+		return Response.status(200).entity(objeto.toString()).build();
+	}
+	
+	private JSONObject buildResponse(String id, String complejidad, String tag) {
+		JSONObject ob = new JSONObject();
+		ob.put("id", id);
+		ob.put("complejidad", complejidad);
+		ob.put("tag", tag);
+		ob.put("ruta", buildArray());
+		return ob;
+		
+	}
+	
+	private JSONArray buildArray() {
+		JSONArray array = new JSONArray();		
+		array.put(buildCoordinate("14/01/2016", "06:00:00", "7.04024", "-73.11703"));
+		array.put(buildCoordinate("14/01/2016", "06:00:00", "7.04054", "-73.11708"));
+		array.put(buildCoordinate("14/01/2016", "06:00:00", "7.04084", "-73.11712"));		
+		return array;
+	}
+	
+	private JSONObject buildCoordinate(String fecha, String hora, String latitud, String longitud) {
+		JSONObject ob = new JSONObject();
+		ob.put("fecha", fecha);
+		ob.put("hora", hora);
+		ob.put("latitud", latitud);
+		ob.put("longitud", longitud);
+		return ob;
+	}
+	
+
+	
+	public Response verifyRESTServiceOldVersion(InputStream incomingData) {
 		String string = "";
 		JSONObject objeto=null;
 		try {
@@ -43,4 +80,5 @@ public class Entrega {
 		// Retorna el objeto
 		return Response.status(200).entity(objeto.toString()).build();
 	}
+	
 }
